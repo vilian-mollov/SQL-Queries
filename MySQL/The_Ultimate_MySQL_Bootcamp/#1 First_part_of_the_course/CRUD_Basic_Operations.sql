f0a26195bcaf1d295d64cfd1db126f48fa8c9044
Avalon <vilian3737@gmail.com>
2023-01-04 18:30:26 +0200

    Moved Create_Database-book_shop.sql in appropriate directory And Added CRUD_Basic_Operations.sql

---
+++ MySQL/The_Ultimate_MySQL_Bootcamp/#1 First_part_of_the_course/CRUD_Basic_Operations.sql
@@ -0,0 +1,17 @@
+INSERT INTO books () VALUES (); -- SKELETON
+
+INSERT INTO books (title, author_fname, author_lname, released_year, stock_quantity, pages) 
+VALUES ('The amazing adventures of Finn and Jake', 'Finn', 'Jackson', 2023, 1754,717);
+SELECT * FROM books;
+
+SELECT * FROM books WHERE book_id > 10;
+
+SELECT book_id AS id, name FROM books;
+
+UPDATE books SET pages = 321 WHERE author_lname = 'Jackson';
+
+DELETE FROM books WHERE title is null;
+
+DELETE FROM books WHERE author_lname = '';
+
+DELETE FROM books WHERE author_lname is null;
