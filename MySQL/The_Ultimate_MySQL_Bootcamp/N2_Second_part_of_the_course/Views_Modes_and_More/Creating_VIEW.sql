0fbd016493fca76ad890c0be824ddde8e51a5444
Avalon <vilian3737@gmail.com>
2023-01-07 16:59:22 +0200

    Added Creating_VIEW.sql

---
+++ MySQL/The_Ultimate_MySQL_Bootcamp/N2_Second_part_of_the_course/Views_Modes_and_More/Creating_VIEW.sql
@@ -0,0 +1,18 @@
+USE `many_to_many_exercise`;
+
+SELECT * FROM reviewers
+JOIN reviews ON reviews.reviewer_id = reviewers.id
+JOIN series ON reviews.series_id = series.id;
+
+-- Creating VIEW 
+CREATE VIEW full_reviews AS 
+SELECT title, released_year, genre, rating, first_name, last_name FROM reviewers 
+JOIN reviews ON reviews.reviewer_id = reviewers.id 
+JOIN series ON reviews.series_id = series.id; 
+
+-- using the virtula table the same why others
+SELECT * FROM full_reviews;
+
+SELECT * FROM full_reviews WHERE genre = 'Comedy';
+
+SELECT * FROM full_reviews ORDER BY title;
