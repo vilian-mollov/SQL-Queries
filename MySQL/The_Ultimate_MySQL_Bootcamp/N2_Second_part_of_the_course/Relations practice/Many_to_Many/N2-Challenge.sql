a0b928a76684d582a522900fb638bb8526311276
Avalon <vilian3737@gmail.com>
2023-01-07 11:04:54 +0200

    Updated N2-Challenge.sql

--- MySQL/The_Ultimate_MySQL_Bootcamp/N2_Second_part_of_the_course/Relations practice/Many_to_Many/N2-Challenge.sql
+++ MySQL/The_Ultimate_MySQL_Bootcamp/N2_Second_part_of_the_course/Relations practice/Many_to_Many/N2-Challenge.sql
@@ -10,3 +10,16 @@ JOIN reviews ON series_id = series.id
 GROUP BY title
 ORDER BY 2;
 
+-- Ordered by average rating highest to lowest limited to 10 results 
+SELECT title, ROUND(AVG(rating), 1) AS avg_rating, COUNT(*) AS numb_of_ratings FROM series
+JOIN reviews ON series_id = series.id
+GROUP BY title
+ORDER BY 2 DESC
+LIMIT 10;
+
+-- Ordered by number of ratings
+SELECT title, ROUND(AVG(rating), 1) AS avg_rating, COUNT(*) AS numb_of_ratings FROM series
+JOIN reviews ON series_id = series.id
+GROUP BY title
+ORDER BY numb_of_ratings DESC
+LIMIT 10;
