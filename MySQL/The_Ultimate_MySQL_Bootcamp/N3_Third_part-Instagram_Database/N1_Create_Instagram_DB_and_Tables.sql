09910754e77b9dfedfcf5da79e8f1615499d8450
Avalon <vilian3737@gmail.com>
2023-01-09 15:17:06 +0200

    Added N1_Create_Instagram_DB_and_Tables.sql first file for Instagram database section

---
+++ MySQL/The_Ultimate_MySQL_Bootcamp/N3_Third_part-Instagram_Database/N1_Create_Instagram_DB_and_Tables.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE instagram_db;
+
+USE instagram_db;
+
+-- needed tables:
+-- users, photos, comments, likes, hashtags, follows
+-- also have to make joining tables
+
+CREATE TABLE users (
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(255) NOT NULL UNIQUE,
+create_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photos(
+id INT PRIMARY KEY AUTO_INCREMENT,
+image_url VARCHAR(255) NOT NULL,
+user_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE likes(
+user_id INT NOT NULL,
+photo_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY (photo_id) REFERENCES photos(id),
+FOREIGN KEY (user_id) REFERENCES users(id),
+PRIMARY KEY(user_id, photo_id)
+);
+
+CREATE TABLE comments(
+id INT PRIMARY KEY AUTO_INCREMENT,
+comment_text VARCHAR(255) NOT NULL,
+user_id INT NOT NULL,
+photo_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY (photo_id) REFERENCES photos(id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE tags(
+id INT PRIMARY KEY AUTO_INCREMENT,
+tag_name VARCHAR(255) NOT NULL UNIQUE,
+created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags (
+photo_id INT NOT NULL,
+tag_id INT NOT NULL,
+FOREIGN KEY (photo_id) REFERENCES photos(id),
+FOREIGN KEY (tag_id) REFERENCES tags(id),
+PRIMARY KEY (photo_id, tag_id)
+);
+
+CREATE TABLE follows(
+follower_id INT NOT NULL,
+folowee_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT NOW(),
+FOREIGN KEY (follower_id) REFERENCES users(id),
+FOREIGN KEY (folowee_id) REFERENCES users(id),
+PRIMARY KEY (follower_id, folowee_id)
+);
+
+
